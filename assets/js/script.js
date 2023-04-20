90fe7bd8c34ce0817dbb145e964cf4e36e6253f6
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-04-20 07:29:04 -0400

    working on nutrtion button got add event listners still a work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
-console.log("hello")
 
+var foodList = document.getElementById
+var FeedMeButton = document.getElementById('dropdown-menu');
 
-fetch("https://api.api-ninjas.com/v1/nutrition")
-.then(function(resposne) {
-    return Response.json();
-})
-.then(function(data) {
-    console.log(data);
+fetch("https://api.api-ninjas.com/v1/nutrition?query=")
+    .then(function (resposne) {
+        return Response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        console.log(data.name);
+        console, log(data.calories);
+    })
+
+FeedMeButton.addEventListener('click', function () {
+    console.log("yum");
 })
